a8a9ed565cf69ca30590eae20e3e14d10020ec37
kumol <39502774+kumol@users.noreply.github.com>
2022-11-10 01:36:14 +0600

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-console.log(process.env.DB_URL)
 mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true},(err)=>{
     if(!err){
         console.log("Database connected");
